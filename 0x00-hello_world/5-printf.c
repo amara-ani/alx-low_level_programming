eb4c267674c4c6355531f93d452794c1318a30f6
amara-ani <amarachukwuani23@gmail.com>
2023-04-21 08:16:14 -0700

    prinf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printf(): to display result
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
