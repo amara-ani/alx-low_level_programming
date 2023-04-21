a42253361b3c8d915cd3448920daf8332b523f1c
amara-ani <amarachukwuani23@gmail.com>
2023-04-21 10:32:16 -0700

    preprocessor

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+/**
+ * main - Entry point
+ * print a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
