2d949910ede1258b803c0c3d8ba395df346c6394
amara-ani <amarachukwuani23@gmail.com>
2023-04-22 10:55:56 -0700

    base 10 singles

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets in lowercase
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 0;
+	while (ch < 10)
+	{
+		printf("%d", ch);
+		ch = ch + 1;
+	}
+	printf("\n");
+	return (0);
+}
