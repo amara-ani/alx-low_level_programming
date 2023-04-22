31c793b87a28877917ccb988365e6b6d9cc48b10
amara-ani <amarachukwuani23@gmail.com>
2023-04-22 10:43:57 -0700

    removing some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing alphabets in lowercase
+ * Return : 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);		
+		}
+		ch = ch + 1;
+	}
+	putchar('\n');
+	return (0);
+}
