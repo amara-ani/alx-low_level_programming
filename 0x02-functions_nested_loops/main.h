cfecbeb65929312cd9395b942b9af123e3b6e4ba
amara-ani <amarachukwuani23@gmail.com>
2023-04-26 03:39:17 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,2 @@
 int _putchar(char c);
-void print_alphabet(void)
-{
-	char ch;
-	
-	ch='a';
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-	_putchar('\n');
-}
-			
+void print_alphabet(void);
