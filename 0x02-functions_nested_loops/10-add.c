f72170da76c6341e8962c783ad8940eb573e92e8
amara-ani <amarachukwuani23@gmail.com>
2023-04-26 17:41:00 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - prints addition of two numbers
+ * @a: checker
+ * @b: another checker
+ * Return: values
+ */
+int add(int a, int b);
+{
+	int a, b, result;
+
+	result = a + b;
+	return result;
+}
