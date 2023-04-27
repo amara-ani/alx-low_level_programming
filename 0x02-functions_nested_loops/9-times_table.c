1f92b92974455dd1230b74548c99304c8437b2d9
amara-ani <amarachukwuani23@gmail.com>
2023-04-27 08:48:14 -0700

    multiples of 9 with _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0');
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -24,7 +24,7 @@ void times_table(void)
 			{
 				_putchar((result / 10) + '0');
 			}
-			_putchar((result % 10) + '0');			
+			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
 	}
