34c77467410b3fe72349931efa4a1617a1d3a2e9
amara-ani <amarachukwuani23@gmail.com>
2023-04-29 15:26:40 -0700

    fizz buzz task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Fizz-Buzz test
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{	
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i == 100)
+		{
+			continue;
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
