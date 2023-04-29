92cb08b54ef08fcae8940f8173c70ad7bae2bdcc
amara-ani <amarachukwuani23@gmail.com>
2023-04-29 06:30:49 -0700

    multiplying two variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - muliplies two integers
+ * @a: checker
+ * @b: checker
+ * Return: 0 (success)
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a *  b;
+	printf("%d", result);
+	return (0);
+}
