7ff4f615fb3e033bc2b530ac43624425abcf5aae
amara-ani <amarachukwuani23@gmail.com>
2023-04-29 08:37:03 -0700

    print 10 times a result

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times of any result
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, count;
+
+	count = 1;
+	while (count <= 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+		_putchar('\n');
+		count++;
+	}
+}
