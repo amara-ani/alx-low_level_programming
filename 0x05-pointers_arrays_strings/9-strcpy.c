42a22f46b4fd7823740085b5d140053e823f35a7
amara-ani <amarachukwuani23@gmail.com>
2023-05-04 11:17:38 -0700

    little changes on 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,14 +7,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int index;
 
-	while (src[index])
+	for (index = 0; src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
+	}
+	while (src[index])	
+	{
+        	dest[index] = '\0';
 		index++;
 	}
-
 	return (dest);
 }
 
