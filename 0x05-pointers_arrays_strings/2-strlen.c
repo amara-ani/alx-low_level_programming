ac270064515aae0c9298ceae4266c71af2d2a4d1
amara-ani <amarachukwuani23@gmail.com>
2023-05-04 07:34:47 -0700

    counts length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - checks the length of a string
+ * @s: checker
+ * Return: value (success)
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != 0; a++)
+	{
+	}
+	return (a);
+}
