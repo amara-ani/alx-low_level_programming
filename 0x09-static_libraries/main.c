183a9b373f3668f1893470f859a8d94b46b07e1b
amara-ani <amarachukwuani23@gmail.com>
2023-05-14 13:11:16 -0700

    fixing betty issue

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
+/**
+ * main - main
+ * Return: void
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be");
+	_puts("the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
