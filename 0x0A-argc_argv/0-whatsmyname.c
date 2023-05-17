539a3520cd0345b9cae7287c53b2ecb5b11926dd
amara-ani <amarachukwuani23@gmail.com>
2023-05-17 15:37:44 -0700

    prints name of array

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the name array
+ * @argc: counts arguments
+ * @argv: holds array of arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s \n", argv[0]);
+	return (0);
+}
