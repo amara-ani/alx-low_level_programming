470e7fbd24e9972bcd84df0ca605a857a4a23bfd
amara-ani <amarachukwuani23@gmail.com>
2023-05-18 00:53:27 -0700

    sum of arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add arguments passed
+ * @argc: counts arguments
+ * @argv: holds array of arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, count;
+
+	count = 0;
+	if (argc < 1)
+	{
+		printf("0 \n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				count += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error \n");
+				return (1);
+			}
+		}
+		printf("%d \n", count);
+	}
+	return (0);
+}
