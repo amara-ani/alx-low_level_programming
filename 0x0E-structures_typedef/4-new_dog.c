1ef933f9d61eb8861c45dc583b147d9d44cab95d
amara-ani <amarachukwuani23@gmail.com>
2023-05-24 15:47:54 -0700

    copying everything in dog.h to the dog_t  even space

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,18 +9,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *puppy;
+	struct dog *new_dog;
 
-	puppy = malloc(sizeof(struct dog));
-	if (puppy == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		puppy->name = name;
-		puppy->age = age;
-		puppy->owner = owner;
-	}
-	return (puppy);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
 }
