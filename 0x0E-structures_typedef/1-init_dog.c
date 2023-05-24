a947256052d706aa2d1b707cf40cad1902466e25
amara-ani <amarachukwuani23@gmail.com>
2023-05-24 07:01:46 -0700

    deference dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog -  function that deferences dog.h
+ * @name: string
+ * @age: age of the string
+ * @d: pointer to the struct-dog address
+ * @owner: owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
