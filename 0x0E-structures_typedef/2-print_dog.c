3e1a06d5e973cf56cb703435e7bd8fe5c96bdd14
amara-ani <amarachukwuani23@gmail.com>
2023-05-24 07:45:13 -0700

    print arguments in dog.h

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,14 +10,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
-		if (d->age)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("Age: (nil)\n");
-		}
+		printf("Age: %f\n", d->age);
 		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 	}
 }
