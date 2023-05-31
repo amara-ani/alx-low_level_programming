9f4345cccf8fb13cfb0720f58307bb72486aa18a
amara-ani <amarachukwuani23@gmail.com>
2023-05-31 02:10:14 -0700

    variadict

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints all the strings including a separator
+ * @separator: character
+ * @n: checker
+ * Return: nothing (fails) or value and separator (success)
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+
+
+	va_start(a, n);
+	for (i = 0; i < n ; i++)
+	{
+		printf("%s", va_arg(a, char *));
+		if (separator == NULL)
+		{
+			printf("nil");
+		}
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(a);
+	printf("\n");
+}
