e063738880ec2ef1bee1959f465916484bc61eda
amara-ani <amarachukwuani23@gmail.com>
2023-06-06 11:25:09 -0700

    print number of elements in list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - number of elements in list_t
+ * @h: holds the contents of list_t
+ * Returns: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t a;
+
+	for (a = 0; h; a++)
+	{
+		h = h->next;
+	}
+	return (a);
+}
