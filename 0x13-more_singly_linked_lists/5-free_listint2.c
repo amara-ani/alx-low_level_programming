22c3567b1164c952a5e4ca329c1f499ffcc7e127
amara-ani <amarachukwuani23@gmail.com>
2023-06-07 06:56:58 -0700

    freed all pull adding a null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees list_t and sets head to NULL
+ * @head: contains what is meant to be freed
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i;
+
+	while (*head)
+	{
+		i = (*head)->next;
+		free(*head);
+		*head = i;
+	}
+	*head = NULL;
+}
