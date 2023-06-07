dc4bafaf0b3e9f34b33d896fbe4436662bae8bfd
amara-ani <amarachukwuani23@gmail.com>
2023-06-07 16:07:07 -0700

    add node to a particular position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 		tmp = tmp->next;
-		idx --;
+		idx--;
 	}
 	new->next = tmp->next;
 	tmp->next = new;
