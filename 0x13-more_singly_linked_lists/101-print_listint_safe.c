4a08876ec0462d745f93dc6425905286023d36fe
amara-ani <amarachukwuani23@gmail.com>
2023-06-07 16:27:02 -0700

    print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * print_listint_safe - listsint_t linked list
+  * @head: checker
+  * Return: number of nodes
+  */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
